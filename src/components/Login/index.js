6cb72df9b366597b8c1438d8f7105e540989af2b
Gary Hsiao <garyh6@users.noreply.github.com>
2020-01-26 00:45:59 -0800

    connect to badauth server

--- src/components/Login/index.js
+++ src/components/Login/index.js
@@ -1,33 +1,41 @@
-import React from "react";
+import React, { useState } from "react";
 import "./Login.scss";
 
 const Login = () => {
+  const [username, setUsername] = useState("");
+  const [password, setPassword] = useState("");
+
   const handleSubmit = async e => {
     e.preventDefault();
-    console.log("************ e", e);
     try {
-      // const res = await fetch(``, {
-      //   method: "POST",
-      //   headers: new Headers({
-      //     "Content-Type": "application/x-www-form-urlencoded"
-      //   }),
-      //   body: {}
-      // });
-      console.log("************ var ");
+      const res = await fetch(`http://localhost:4000/login`, {
+        method: "POST",
+        headers: new Headers({
+          "Content-Type": "application/json"
+        }),
+        body: JSON.stringify({
+          username,
+          password
+        })
+      });
+      console.log("************ var ", res);
     } catch (err) {
       console.log("************ err", err);
     }
   };
 
   return (
-    <form className="login-box" onSubmit={handleSubmit} autoComplete="off">
+    <form className="login-box" onSubmit={handleSubmit}>
       <h1>Login</h1>
-      <input type="text" name="" placeholder="Username" autoComplete="off" />
+      <input
+        type="text"
+        placeholder="Username"
+        onChange={e => setUsername(e.target.value)}
+      />
       <input
         type="password"
-        name=""
         placeholder="Password"
-        autoComplete="off"
+        onChange={e => setPassword(e.target.value)}
       />
       <button type="submit">Login</button>
     </form>
